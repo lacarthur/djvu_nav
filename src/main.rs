fffb8a940a70b97f48d896a4db6540cc619e74f1
Arthur Lacoin <arthur.lacoin@protonmail.com>
2023-12-22 22:27:05 +0100

    add command line argument for input file

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,19 @@ use crate::app::App;
 
 use std::{io, time::Duration};
 
+use clap::{Command, Arg};
+
 fn main() -> Result<(), io::Error> {
-    let filename = "resources/todo.djvu";
+    let cmd = Command::new("nav_edit")
+        .version("1.0.0")
+        .about("Edit NAV section of djvu files.")
+        .arg(
+            Arg::new("filename")
+                .required(true)
+        );
+
+    let args = cmd.get_matches();
+    let filename = args.get_one::<String>("filename").unwrap();
     let tick_rate = Duration::from_millis(250);
     match App::new(filename) {
         Ok(mut application) => {
